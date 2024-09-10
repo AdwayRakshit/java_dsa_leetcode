d5bc4b2407453b2a58e96555b7e167d773937997
Adway Rakshit <72248673+AdwayRakshit@users.noreply.github.com>
2024-09-10 18:06:50 +0530

    Time: 1 ms (99.82%), Space: 46.1 MB (35.86%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       int sum=0;
-       HashSet<Integer> set=new HashSet<>();
-       for(int i=0;i<nums.length;i++){
-        if(set.contains(nums[i])){
-            sum-=nums[i];
+     int n=nums.length;
+     int result=0;
+        for(int i=0;i<n;i++){
+            result^=nums[i];
         }
-        else{
-            sum+=nums[i];
-            set.add(nums[i]);
-        }
-    
-       } 
-       return sum;
+        return result;
     }
 }
